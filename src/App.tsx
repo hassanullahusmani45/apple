cfd06387354db5edea6ac9a9c6d0573a98cdcffd
hassanullahusmani45 <hassanullahusmani45@gmail.com>
2025-08-06 23:47:05 +0430

    Setup the project

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+
+function App() {
+  return (
+    <div id="top" className="container p-4 sm:p[2rem] mx-auto">
+      Hassan
+      <div className="text-red-500 dark:text-amber-300">
+        USMANI
+      </div>
+    </div>
+  )
+}
+
+export default App
