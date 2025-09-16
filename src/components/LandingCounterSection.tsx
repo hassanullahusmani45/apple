6a9a3bbdf365e3e7a82f54a9cff7e412462edc19
Hassanullah Usmani <hassanullahusmani45@gmail.com>
2025-09-16 16:43:12 +0430

    localization of About page and LandingCounterSection component.

--- src/components/LandingCounterSection.tsx
+++ src/components/LandingCounterSection.tsx
@@ -1,26 +1,28 @@
 import { HiOutlineAcademicCap, HiOutlineClipboardDocumentList, HiOutlineUsers } from 'react-icons/hi2';
 import LandingCounter from './LandingCounter';
 import type { LandingCounterSectionProps } from '../types/type';
+import { useTranslation } from 'react-i18next';
 
 
 
 export default function LandingCounterSection({ teamMembers, articles, subscribers }: LandingCounterSectionProps) {
+    const { t } = useTranslation("main")
     return (
         <>
             <div className='flex flex-col justify-center items-center'>
                 <HiOutlineUsers className='size-9 md:size-12 mb-1 md:mb-3' />
                 <LandingCounter count={teamMembers} />
-                <div className='text-sm md:text-base font-semibold'>Team Members</div>
+                <div className='text-sm md:text-base font-semibold'>{t("Team Members")}</div>
             </div>
             <div className='flex flex-col justify-center items-center'>
                 <HiOutlineClipboardDocumentList className='size-9 md:size-12 mb-1 md:mb-3' />
                 <LandingCounter count={articles} />
-                <div className='text-sm md:text-base font-semibold'>Total Articles</div>
+                <div className='text-sm md:text-base font-semibold'>{t("Total Articles")}</div>
             </div>
             <div className='flex flex-col justify-center items-center'>
                 <HiOutlineAcademicCap className='size-9 md:size-12 mb-1 md:mb-3' />
                 <LandingCounter count={subscribers} />
-                <div className='text-sm md:text-base font-semibold'>Users</div>
+                <div className='text-sm md:text-base font-semibold'>{t("Users")}</div>
             </div>
         </>
     )
--- src/pages/about/About.tsx
+++ src/pages/about/About.tsx
@@ -4,10 +4,11 @@ import { useAppSelector } from '../../hooks/reduxHooks'
 import LandingCounterSection from '../../components/LandingCounterSection'
 import TeamMemmberCard from '../../components/TeamMemmberCard';
 import AboutSkeleton from '../../components/skeleton/AboutSkeleton';
+import { useTranslation } from 'react-i18next';
 
 
 export default function About() {
-
+    const { t }=useTranslation('main')
     const { teamMembers, loading } = useAppSelector((state) => state.home);
     const { teamMembers: countTeamMembers, articles, subscribers, loading: countLoading } = useAppSelector(state => state.stats)
 
@@ -20,17 +21,17 @@ export default function About() {
                         <img src={aboutImage} alt='abute' className='bg-cover w-[80%] sm:w[60%] md:w-full xl:w-[90%]' />
                     </div>
                     <div className='col-span-2 md:col-span-1 mt-10 lg:mt-32 px-3 sm:p-0'>
-                        <div className='text-2xl font-semibold leading-10'>Apple The Best<br /> Place For learning Articles</div>
+                        <div className='text-2xl font-semibold leading-10'>{t("Apple Tecnology The Best")}<br /> {t("Place For learning Articles")}</div>
                         <div className='mt-5 mb-5 lg:mb-16 pl-6 text-slate-900 dark:text-slate-200 leading-7'>
-                            Apple is the best place for learning articles, offering insightful and high-quality content to help you stay ahead in technology and beyond. Whether you are a beginner or an expert, our articles are designed to inspire and educate, empowering you to expand your knowledge and skills.
+                            {t("apple_info")}
                         </div>
-                        <Link to='/contact-us' className='button-style'>Contact Us</Link>
+                        <Link to='/contact-us' className='button-style'>{t("Contact Us")}</Link>
                     </div>
                 </div>
 
                 {loading || countLoading ? (<AboutSkeleton />) :
                     <>
-                        <div className='mt-15 md:mt-20 lg:mt-40 text-center font-semibold text-xl'>Our Experienced Team</div>
+                        <div className='mt-15 md:mt-20 lg:mt-40 text-center font-semibold text-xl'>{t("Our Experienced Team")}</div>
                         <div className='w-2/3 sm:w-1/2 md:w-1/3  xl:w-1/5 mx-auto mt-1 border-t-2 border-dotted border-teal-300 ' ></div>
 
                         <div className='grid grid-cols-12 sm:gap-6 xl:gap-x-3 2xl:gap-x-12 my-8 xl:my-16'>
@@ -53,7 +54,7 @@ export default function About() {
                         </div>
 
 
-                        <div className='mt-15 xl:mt-20 text-center font-semibold text-xl text-nowrap'>Surprise Information About Our Web Site</div>
+                        <div className='mt-15 xl:mt-20 text-center font-semibold text-xl text-nowrap'>{t("Surprise Information About Our Web Site")}</div>
                         <div className='w-10/11 md:w-4/6 lg:w-1/2 xl:w-1/3 mx-auto mt-1 border-t-2 border-dotted border-teal-300 ' ></div>
 
                         <div className='flex justify-around items-center lg:w-1/2 my-12 lg:mx-auto'>
