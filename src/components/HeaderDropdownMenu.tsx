40cf64f3ab2638a047ef3bd6cc95db64d91e8717
Hassanullah Usmani <hassanullahusmani45@gmail.com>
2025-09-27 09:34:06 +0430

    update background of HeaderDropdownMenu.

--- src/components/HeaderDropdownMenu.tsx
+++ src/components/HeaderDropdownMenu.tsx
@@ -22,7 +22,7 @@ export default function HeaderDropdownMenu({ children,icon }: HeaderDropdownMenu
             </div>
 
             {open && (
-                <div className="absolute end-0 bg-slate-200 dark:bg-gray-900 border-x rounded-lg px-2 py-3 lg:px-6 lg:py-4 space-y-2 md:space-y-3 shadow-lg">
+                <div className="absolute end-0 bg-gradient-to-b from-slate-100 to-slate-300   dark:from-gray-900 dark:to-gray-800 border-x-2 rounded-lg px-2 py-3 lg:px-6 lg:py-4 space-y-2 md:space-y-3 shadow-lg">
                     {children}
                 </div>
             )}
