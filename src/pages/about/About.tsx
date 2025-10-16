bc26b92554b9876f87dc773da3622104f701486e
Hassanullah Usmani <hassanullahusmani45@gmail.com>
2025-10-16 21:09:38 +0430

    The animations of About page.

--- src/pages/about/About.tsx
+++ src/pages/about/About.tsx
@@ -5,12 +5,41 @@ import LandingCounterSection from '../../components/LandingCounterSection'
 import TeamMemmberCard from '../../components/TeamMemmberCard';
 import AboutSkeleton from '../../components/skeleton/AboutSkeleton';
 import { useTranslation } from 'react-i18next';
+import { useLayoutEffect } from 'react';
+import gsap from 'gsap';
+import { ScrollTrigger } from 'gsap/all';
 
 
 export default function About() {
-    const { t }=useTranslation('main')
+    const { t } = useTranslation('main')
     const { teamMembers, loading } = useAppSelector((state) => state.home);
     const { teamMembers: countTeamMembers, articles, subscribers, loading: countLoading } = useAppSelector(state => state.stats)
+
+    useLayoutEffect(() => {
+
+
+        gsap.set(".teamMember-item", { opacity: 0, y: 100 });
+        ScrollTrigger.batch(".teamMember-item", {
+            interval: 0.3,
+            batchMax: 7,
+            onEnter: batch => gsap.to(batch, {
+                opacity: 1,
+                scale: 1,
+                y: 0,
+                stagger: 0.15,
+                duration: 0.8,
+                ease: 'back'
+            }),
+            onLeaveBack: batch => gsap.to(batch, {
+                scale: 0.5,
+                y: 100,
+                opacity: 0,
+                duration: 0.8
+            }),
+            start: "top 75%",
+        });
+
+    }, [loading, teamMembers]);
 
     return (
         <>
@@ -37,7 +66,7 @@ export default function About() {
                         <div className='grid grid-cols-12 sm:gap-6 xl:gap-x-3 2xl:gap-x-12 my-8 xl:my-16'>
 
                             {teamMembers.map(({ id, profile, emaillink, linkedinlink, weblink, first_name, last_name, position, info }) => (
-                                <div key={id} className='col-span-12 sm:col-span-6 lg:col-span-4 xl:col-span-3 mx-10 sm:m-0'>
+                                <div key={id} className='teamMember-item col-span-12 sm:col-span-6 lg:col-span-4 xl:col-span-3 mx-10 sm:m-0'>
                                     <TeamMemmberCard
                                         id={id}
                                         profile={profile}
